a2425e83767d4778fcc2252590a0013ecf0b0fa5
MasanaIto <z3heng0326@gmail.com>
2019-12-09 21:58:20 +0900

    休日の編集機能を追加

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -8,26 +8,6 @@ use Illuminate\Http\Request;
 
 class CalendarController extends Controller
 {
-    public function getHoliday(Request $request)
-    {
-        // 休日データ取得
-        $list = Holiday::all();
-        return view('calendar.holiday', ['list' => $list]);
-    }
-
-    public function postHoliday(Request $request)
-    {
-        // POSTで受信した休日データの登録
-        $holiday = new Holiday();
-        $holiday->day = $request->day;
-        $holiday->description = $request->description;
-        $holiday->save();
-
-        //休日データ取得
-        $list = Holiday::all();
-        return view('calendar.holiday', ['list' => $list]);
-    }
-
     public function index(Request $request)
     {
         $list = Holiday::all();
@@ -35,5 +15,51 @@ class CalendarController extends Controller
         $tag = $cal->showCalendarTag($request->month, $request->year);
 
         return view('calendar.index', ['cal_tag' => $tag]);
+    }
+
+
+
+    public function getHoliday(Request $request)
+    {
+        // 休日データ取得
+        $data = new Holiday();
+        $list = Holiday::all();
+        return view('calendar.holiday', ['list' => $list, 'data' => $data]);
+    }
+
+
+
+    public function getHolidayId($id)
+    {
+        // 休日データ取得
+        $data = new Holiday();
+        if (isset($id)) {
+            $data = Holiday::where('id', '=', $id)->first();
+        }
+        $list = Holiday::all();
+        return view('calendar.holiday', ['list' => $list, 'data' => $data]);
+    }
+
+
+
+    public function postHoliday(Request $request)
+    {
+        // POSTで受信した休日データの登録
+        if (isset($request->id)) {
+            // POSTで受信した休日データの登録
+            $holiday = Holiday::where('id', '=', $request->id)->first();
+            $holiday->day = $request->day;
+            $holiday->description = $request->description;
+            $holiday->save();
+        } else {
+            $holiday = new Holiday();
+            $holiday->day = $request->day;
+            $holiday->description = $request->description;
+            $holiday->save();
+        }
+        //休日データ取得
+        $data = new Holiday();
+        $list = Holiday::all();
+        return view('calendar.holiday', ['list' => $list, 'data' => $data]);
     }
 }
--- resources/views/calendar/holiday.blade.php
+++ resources/views/calendar/holiday.blade.php
@@ -3,16 +3,17 @@
 @section('content')
     <h1>休日設定</h1>
     <!-- 休日入力フォーム -->
-    <form method="POST" action="/holiday"> 
-        <div class="form-group">
-            {{csrf_field()}}
-            <label for="day">日付[YYYY/MM/DD] </label>
-            <input type="text" name="day" class="form-control" id="day">
-            <label for="description">説明</label>
-            <input type="text" name="description" class="form-control" id="description"> 
-        </div>
+<form method="POST" action="/holiday"> 
+    <div class="form-group">
+        {{csrf_field()}}    
+        <label for="day">日付[YYYY/MM/DD] </label>
+        <input type="text" name="day" class="form-control" id="day" value="{{ $data->day }}">
+        <label for="description">説明</label>
+        <input type="text" name="description" class="form-control" id="description" value="{{ $data->description }}"> 
+    </div>
         <button type="submit" class="btn btn-primary">登録</button> 
-    </form>
+        <input type="hidden" name="id" value="{{ $data->id }}">
+</form>
     <a href="{{ url('/') }}">カレンダーに戻る</a>
 
     <!-- 休日一覧表示 -->
@@ -28,7 +29,7 @@
         <tbody>
             @foreach($list as $val)
             <tr>
-                <th scope="row">{{$val->day}}</th>
+                <th scope="row"><a href="{{ url('/holiday/'.$val->id) }}">{{ $val->day }}</a></th>
                 <td>{{$val->description}}</td>
                 <td>{{$val->created_at}}</td>
                 <td>{{$val->updated_at}}</td>
